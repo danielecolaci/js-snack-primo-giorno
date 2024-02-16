18cebb7aa8ab5aa1e264cedf994e873b807d406e
danielecolaci <danielecolaci@outlook.com>
2024-02-16 16:00:48 +0100

    Fix File and Folder

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+//Esercizio 1
+//Dichiara una variabile chiamata 'number' e assegnale il valore 100. Stampa il valore della variabile in console.
+
+let number = 100; //number
+console.log(number);
+
+//Esercizio 2
+//Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+
+const PI = 3.14; //number
+console.log(PI);
